bc8d20c90297ce375073c185fc21a7fed4af8b9d
KAP <KAP@DESKTOP-DNB9NIU>
2021-01-27 17:15:59 +0300

    поиск

--- AcademicCenter/Forms/FormComplex.cs
+++ AcademicCenter/Forms/FormComplex.cs
@@ -1,5 +1,6 @@
 ﻿using AcademicCenter.Properties;
 using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace AcademicCenter
@@ -12,12 +13,26 @@ namespace AcademicCenter
             this.Text += Settings.Default.Discipline;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
+        private void Form1_Load(object sender, EventArgs e) => Menu();
         private void exit_Click(object sender, EventArgs e) => Close();
         private void settings_Click(object sender, EventArgs e) => new FormSettings().ShowDialog();
+
+        private void Menu()
+        {
+            var disc = GetDisciplines();
+            if(disc.Length==0)
+            {
+                if (MessageBox.Show("Нет доступных дисциплин и тестов к ним, создать по умолчанию?", "Внимание...", 
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Warning) != DialogResult.Yes) return;
+                ;
+            }
+        }
+
+        /// <summary> Все дисциплины </summary>
+        private string[] GetDisciplines()
+        {
+            Directory.CreateDirectory(JsonSettings.FolderPath + "\\discipline\\");
+            return Directory.GetDirectories(JsonSettings.FolderPath + "\\discipline\\", "*", SearchOption.TopDirectoryOnly);
+        }
     }
 }
