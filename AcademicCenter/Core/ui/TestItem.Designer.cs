19c2d9698be9a5b27be7bb6ab86502292bc8bdbb
Алексей <sysadkrd@gmail.com>
2021-01-27 22:53:30 +0300

    Test Item Control

---
+++ AcademicCenter/Core/ui/TestItem.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace AcademicCenter
+{
+    partial class TestItem
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lText = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lText
+            // 
+            this.lText.AutoEllipsis = true;
+            this.lText.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lText.Location = new System.Drawing.Point(20, 10);
+            this.lText.Name = "lText";
+            this.lText.Size = new System.Drawing.Size(546, 65);
+            this.lText.TabIndex = 0;
+            this.lText.Text = "Наиболее полно, правильно перечислены характеристики компьютерной сети в списке:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.checkBox1.Location = new System.Drawing.Point(20, 75);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Padding = new System.Windows.Forms.Padding(10);
+            this.checkBox1.Size = new System.Drawing.Size(546, 44);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(485, 219);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ОК";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // TestItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.lText);
+            this.Name = "TestItem";
+            this.Padding = new System.Windows.Forms.Padding(20, 10, 20, 10);
+            this.Size = new System.Drawing.Size(586, 260);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lText;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ AcademicCenter/Core/ui/TestItem.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows.Forms;
+
+namespace AcademicCenter
+{
+    public partial class TestItem : UserControl
+    {
+        private Test.Item item;
+        public TestItem(Test.Item itm)
+        {
+            item = itm;
+            InitializeComponent();
+            lText.Text = item.Question;
+            Collapse();
+            if(item.Answers.Count>0)
+                foreach (Test.Answer answer in item.Answers)
+                {
+                    Controls.Add(new CheckBox
+                    {
+                        Text = item.Question, 
+                        Checked = false, 
+                        Padding = new Padding(10), 
+                        Dock = DockStyle.Top
+                    });
+                }
+        }
+
+        private void Expand()
+        {
+            Height = 
+                
+                button1.Visible = true;
+        }
+
+        private void Collapse()
+        {
+            Height = lText.Height + Padding.Top;
+            button1.Visible = false;
+        }
+    }
+}
