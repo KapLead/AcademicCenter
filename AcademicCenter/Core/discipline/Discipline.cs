c5b0c46d9209605bb9be15f226f343e6d28b88a4
KAP <KAP@DESKTOP-DNB9NIU>
2021-01-27 12:40:16 +0300

    add Discipline

---
+++ AcademicCenter/Core/discipline/Discipline.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AcademicCenter.Core.discipline
+{
+    /// <summary> Дисциплина </summary>
+    public class Discipline
+    {
+
+        /// <summary> наименование дисциплины </summary>
+        public string Name { get; set; }
+
+        /// <summary> Тесты по дисциплине </summary>
+        public List<Test> Tests { get; set; } = new List<Test>();
+
+        public IEnumerable<Document> Documents 
+        { get
+            {
+                List<Document> ret = new List<Document>();
+                foreach (var test in Tests)
+                    foreach(var ans in test.Items)
+                        foreach(var a in ans.Answers)
+                            ret.AddRange(a.Documents);
+                return Distict(ret);
+            } 
+        }
+
+        public IEnumerable<T> Distict<T>(IEnumerable<T> source)
+        {
+            List<T> uniques = new List<T>();
+            foreach (T item in source)
+                if (!uniques.Contains(item)) uniques.Add(item);
+            return uniques;
+        }
+    }
+}
