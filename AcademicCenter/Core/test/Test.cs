f9ad71ca3a708e4855ca467d5979fea6400afc80
KAP <KAP@DESKTOP-DNB9NIU>
2021-01-27 10:41:01 +0300

    add test

---
+++ AcademicCenter/Core/test/Test.cs
@@ -0,0 +1,14 @@
+﻿namespace AcademicCenter.Core.test
+{
+    public class Test
+    {
+
+
+        public class Item
+        {
+            public Type Type { get; set; }
+        }
+
+        public enum Type { Обучение, Контрольная }
+    }
+}
