3a721c459664396206b39cef91199a078c0774aa
KAP <KAP@DESKTOP-DNB9NIU>
2021-01-27 11:13:13 +0300

    test : Item and Answer

--- AcademicCenter/Core/test/Test.cs
+++ AcademicCenter/Core/test/Test.cs
@@ -1,14 +1,45 @@
-﻿namespace AcademicCenter.Core.test
+﻿using System.Collections.Generic;
+
+namespace AcademicCenter.Core.test
 {
     public class Test
     {
+        /// <summary> Содержимое теста </summary>
+        public List<Item> Items { get; set; } = new List<Item>();
+            
+        /// <summary> Вид теста </summary>
+        public Type type { get; set; }
+        
 
 
+  
+        
+        
         public class Item
         {
-            public Type Type { get; set; }
+            /// <summary> Вопрос </summary>
+            public string Question { get; set; }
+           
+            /// <summary> Ответы </summary>
+            public List<Answer> Answers { get; set; }
+
         }
 
+        /// <summary> Виды тестов </summary>
         public enum Type { Обучение, Контрольная }
+
+        /// <summary> Ответ </summary>
+        public class Answer
+        {
+
+            /// <summary> Текст ответа </summary>
+            public string Text { get; set; }
+            
+            /// <summary> Документ для ответа </summary>
+            public List<Document> Documents { get; set; }
+            
+            /// <summary> Является правильным ответом </summary>
+            public bool IsCorrect { get; set; }
+        }
     }
 }
